945a405afeec46f81454c4300239e1a0fb2e5e13
Chong Shao <cshao@cs.unc.edu>
2015-12-29 10:49:01 -0500

    Initial Commit

---
+++ BeaconImageReaderEddy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconImageReaderEddy
+//
+//  Created by Chong Shao on 12/29/15.
+//  Copyright © 2015 Chong Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeaconImageReaderEddy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconImageReaderEddy
+//
+//  Created by Chong Shao on 12/29/15.
+//  Copyright © 2015 Chong Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
